a04b024f2b6b05f190ee0b5cd9282b5cdd3a65eb
Sarah <sarah.pttch@gmail.com>
2025-11-21 22:23:48 +0100

    fixed navigation highlight bug

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,41 +1,31 @@
 import '../styles/Navigation.css'
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
-import CreateOverlay from "./CreateOverlay.jsx"
-import { useCategoryStore } from "../stores/categoryStore.jsx"
+import CreateOverlay from "./CreateOverlay"
+import { useCategoryStore } from "../stores/categoryStore"
 
 export default function Navigation() {
 
-    const [activePage, setActivePage] = useState(0);
     const navigate = useNavigate();
+    const location = useLocation();
 
     const toOverview = () => {
-        setActivePage(0);
         navigate('/');
     }
 
-    // const toCalendar = () => {
-    //     setActivePage(1);
-    //     navigate('/calendar');
-    // }
-
     const toLongterm = () => {
-        setActivePage(1);
         navigate('/longterm')
     }
 
     const toArchive = () => {
-        setActivePage(2);
         navigate('/archive');
     }
 
     const toStatistics = () => {
-        setActivePage(3);
         navigate('/statistics');
     }
 
     const toSettings = () => {
-        setActivePage(4);
         navigate('/settings');
     }
 
@@ -49,32 +39,31 @@ export default function Navigation() {
     return (
         <div className='navigation'>
             <button
-                className={`navButton ${activePage === 0 ? 'active' : ''}`}
+                className={`navButton ${location.pathname === '/' ? 'active' : ''}`}
                 onClick={toOverview}
             >
                 Task Overview
             </button>
-            {/*<button className={`navButton ${activePage === 1 ? 'active' : ''}`} onClick={toCalendar}>Calendar</button>*/}
             <button
-                className={`navButton ${activePage === 1 ? 'active' : ''}`}
+                className={`navButton ${location.pathname === '/longterm' ? 'active' : ''}`}
                 onClick={toLongterm}
             >
                 Longterm Tasks
             </button>
             <button
-                className={`navButton ${activePage === 2 ? 'active' : ''}`}
+                className={`navButton ${location.pathname === '/archive' ? 'active' : ''}`}
                 onClick={toArchive}
             >
                 Archive
             </button>
             <button
-                className={`navButton ${activePage === 3 ? 'active' : ''}`}
+                className={`navButton ${location.pathname === '/statistics' ? 'active' : ''}`}
                 onClick={toStatistics}
             >
                 Statistics
             </button>
             <button
-                className={`navButton ${activePage === 4 ? 'active' : ''}`}
+                className={`navButton ${location.pathname === '/settings' ? 'active' : ''}`}
                 onClick={toSettings}
             >
                 Settings
